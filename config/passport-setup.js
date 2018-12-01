a84c883b8bf5ed282cb5c80b7e03c0b565340401
Avogardo <avogardo0@gmail.com>
2018-12-01 16:50:54 +0100

    save new user in database on login

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./keys');
+const User = require('../models/user-model');
 
 passport.use(
   new GoogleStrategy({
@@ -10,6 +11,13 @@ passport.use(
     callbackURL: '/auth/google/redirect',
   }, (accessToken, refreshToken, profile, done) => {
     // passport callback function
-    console.log('redirect fire', profile);
+    console.log('User profile', profile);
+    new User({
+      googleId: profile.id,
+      userName: profile.displayName,
+      imageUrl: profile.photos[0].value,
+    }).save().then((newUser) => {
+      console.log('new user created', newUser);
+    });
   }),
 );
