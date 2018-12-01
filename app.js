eb7dafb72ff46b14010affa7bdfd235caf3d6f7e
Avogardo <avogardo0@gmail.com>
2018-12-02 00:01:16 +0100

    ass cookie session handler

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require('express');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const authRoutes = require('./routes/auth-routes');
 const passportSetup = require('./config/passport-setup');
 const mongoose = require('mongoose');
+const keys = require('./config/keys');
 
 const app = express();
 
 // set view engine
 app.set('view engine', 'ejs');
+
+// set up session cookies
+app.use(cookieSession({
+  maxAge: 24 * 60 * 60 * 1000, // 1 day: 24h 60min 60sec 1000milisec
+  keys: [keys.session.cookieKey],
+}));
+
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 // connect to mongodb
 mongoose.connect('mongodb://localhost:27017/oauth-app', { useNewUrlParser: true }, () => {
--- config/keys.example.js
+++ config/keys.example.js
@@ -2,5 +2,8 @@ module.exports = {
   google: {
     clientID: 'enter your client id here',
     clientSecret: 'enter your client secret here'
-  }
+  },
+  session: {
+    cookieKey: 'enter your cookie session key',
+  },
 };
