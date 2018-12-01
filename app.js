f2f12f223a736d425b5ca3ec3b0e0c09d29527ca
Avogardo <avogardo0@gmail.com>
2018-12-01 16:33:54 +0100

    add mongodb connection

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const authRoutes = require('./routes/auth-routes');
 const passportSetup = require('./config/passport-setup');
+const mongoose = require('mongoose');
 
 const app = express();
 
 // set view engine
 app.set('view engine', 'ejs');
+
+// connect to mongodb
+mongoose.connect('mongodb://localhost:27017/oauth-app', { useNewUrlParser: true }, () => {
+  console.log('connected to mongodb');
+});
 
 // set up routes
 app.use('/auth', authRoutes);
